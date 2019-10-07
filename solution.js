0db1fb2cdfed0341f47a464d984a7e5d455273f9
DepecheCode <jenna.schroeder@digitalcareerinstitute.org>
2019-10-07 18:15:29 +0200

    populate README

---
+++ solution.js
@@ -0,0 +1,37 @@
+// 1. Number Within Range
+
+//     function withinRange(num, obj) {
+//        if (num >= obj.min && num <= obj.max) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//     }
+
+//     console.log(withinRange(4, { min: 4, max: 5 }));
+
+//     2. Return Keys and Values
+
+//     function returnKeysValues(obj) {
+//         let keyArr = Object.keys(obj);
+//         let valueArr = Object.values(obj);
+//         return [keyArr, valueArr];
+//     }
+
+//    console.log(returnKeysValues({ a: 1, b: 2, c: 3 }));
+
+//     3. Scrabble
+
+// let scrabble = {
+//     tile: "N",
+//     score: 1
+// },
+//     {
+//         tile: "K",
+//         score: 5
+//         },
+//     {
+//         tile: "Z",
+//         score: 10
+//         },
+//     { tile: "X", score: 8 }, { tile: "D", score: 2 }, { tile: "A", score: 1 }, { tile: "E", score: 1 }q
